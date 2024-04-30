00947b608b28f318614bf9593a52363dd9ad8e7a
Shifat-project <shifat32153@gmail.com>
2024-04-30 18:33:04 +0600

    allowed host

--- Navena_Hospital/settings.py
+++ Navena_Hospital/settings.py
@@ -125,6 +125,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://novena-hospital-dqxr.onrender.com','https://*.127.0.0.1']
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
